97aeb9b10384252eb02655366c06ab436a54e809
Niels van Huijstee <niels@huijs.net>
2022-04-27 11:22:35 +0200

    Switch null-ls python sources to project pyenv.

--- nvim/.config/nvim/lua/plugins/null-ls-config.lua
+++ nvim/.config/nvim/lua/plugins/null-ls-config.lua
@@ -7,24 +7,28 @@ local function run_from_nvim_pyenv(command_name)
 	local command_full_path = HOME .. "/.pyenv/versions/nvim/bin/" .. command_name
 	return command_full_path
 end
+local function run_from_active_pyenv(command_name)
+	local command_full_path = HOME .. "/.pyenv/versions/nvim/bin/" .. command_name
+	return command_full_path
+end
 
 local active_pyenv_python = HOME .. "/.pyenv/shims/python"
 null_ls.setup({
 	diagnostics_format = "[#{s}|#{c}] #{m}",
 	sources = {
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.diagnostics.mypy.with({
-			command = run_from_nvim_pyenv("mypy"),
+        null_ls.builtins.diagnostics.mypy.with({
+			command = run_from_active_pyenv("mypy"),
 			extra_args = { "--python-executable", active_pyenv_python },
 		}),
 		null_ls.builtins.diagnostics.pydocstyle.with({
-			command = run_from_nvim_pyenv("pydocstyle"),
+			command = run_from_active_pyenv("pydocstyle"),
 			extra_args = { "--convention", "numpy" },
 		}),
-		null_ls.builtins.diagnostics.flake8.with({ command = run_from_nvim_pyenv("flake8") }),
-		null_ls.builtins.formatting.black.with({ command = run_from_nvim_pyenv("black") }),
+		null_ls.builtins.diagnostics.flake8.with({ command = run_from_active_pyenv("flake8") }),
+		null_ls.builtins.formatting.black.with({ command = run_from_active_pyenv("black") }),
 		null_ls.builtins.formatting.isort.with({
-			command = run_from_nvim_pyenv("isort"),
+			command = run_from_active_pyenv("isort"),
 			extra_args = { "--profile", "black" },
 		}),
 		null_ls.builtins.formatting.stylua,
